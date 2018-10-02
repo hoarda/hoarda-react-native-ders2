d33c7b602d0f1b1cd6004ce26dc9e08b1f8c1710
Oktay Arda <hasanoktayarda@gmail.com>
2018-10-02 10:20:44 +0200

    ekrana text yazdirma

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { Text, View } from "react-native";
 
-export default class App extends React.Component {
+export default class HelloWorldApp extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Text>Hello world!</Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
