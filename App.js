ea8b47c08483b716b0d9a52552703e09f6d5dfc3
Oktay Arda <hasanoktayarda@gmail.com>
2018-10-09 21:52:41 +0300

    scrool komutu

--- App.js
+++ App.js
@@ -1,86 +1,184 @@
 import React, { Component } from "react";
-import {
-  Alert,
-  AppRegistry,
-  Platform,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  TouchableOpacity,
-  TouchableNativeFeedback,
-  TouchableWithoutFeedback,
-  View
-} from "react-native";
+import { AppRegistry, ScrollView, Image, Text } from "react-native";
 
-export default class Touchables extends Component {
-  _onPressButton() {
-    Alert.alert("You tapped the button!");
-  }
-
-  _onLongPressButton() {
-    Alert.alert("You long-pressed the button!");
-  }
-
+export default class IScrolledDownAndWhatHappenedNextShockedMe extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableHighlight</Text>
-          </View>
-        </TouchableHighlight>
-        <TouchableOpacity onPress={this._onPressButton}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableOpacity</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableNativeFeedback
-          onPress={this._onPressButton}
-          background={
-            Platform.OS === "ios"
-              ? TouchableNativeFeedback.SelectableBackground()
-              : ""
-          }
-        >
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableNativeFeedback</Text>
-          </View>
-        </TouchableNativeFeedback>
-        <TouchableWithoutFeedback onPress={this._onPressButton}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
-          </View>
-        </TouchableWithoutFeedback>
-        <TouchableHighlight
-          onPress={this._onPressButton}
-          onLongPress={this._onLongPressButton}
-          underlayColor="white"
-        >
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>Touchable with Long Press</Text>
-          </View>
-        </TouchableHighlight>
-      </View>
+      <ScrollView>
+        <Text style={{ fontSize: 55 }}>Ekrani Kaydırın</Text>
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 50,
+            height: 50
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 50,
+            height: 50
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 50,
+            height: 50
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 50,
+            height: 50
+          }}
+        />
+        <Text style={{ fontSize: 96 }}>Sevdin mi hınzır</Text>
+
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 50,
+            height: 50
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 50,
+            height: 50
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 50,
+            height: 50
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 50,
+            height: 50
+          }}
+        />
+        <Text style={{ fontSize: 96 }}>Devam et</Text>
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Text style={{ fontSize: 96 }}>iyi mi??</Text>
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Text style={{ fontSize: 96 }}>;)</Text>
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Text style={{ fontSize: 80 }}>React Native</Text>
+      </ScrollView>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 60,
-    alignItems: "center"
-  },
-  button: {
-    marginBottom: 30,
-    width: 260,
-    alignItems: "center",
-    backgroundColor: "#2196F3"
-  },
-  buttonText: {
-    padding: 20,
-    color: "white"
-  }
-});
-
-// skip this line if using Create React Native App
-AppRegistry.registerComponent("AwesomeProject", () => Touchables);
+AppRegistry.registerComponent(
+  "AwesomeProject",
+  () => IScrolledDownAndWhatHappenedNextShockedMe
+);
